8f1efc16e93e2fbe1df7823cc69f9294e1803e4d
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-25 16:31:16 +0500

    Save data in array and redux store

--- src/components/AddData.js
+++ src/components/AddData.js
@@ -6,43 +6,6 @@ const AddData = () => {
 
     const dispatch = useDispatch();
 
-    /*
-    const [name, setName] = useState('')
-    const [age, setAge] = useState('')
-    const [address, setAddress] = useState('')
-
-    const [state, setState] = useState({
-        name: '',
-        age: '',
-        address: ''
-    })
-
-    const handleInputs = e => {
-        setState({ ...state, [e.target.name]: e.target.value }); //...state means all states
-    }
-
-
-    const addData = () => {
-        let arr = [];
-        let data = {
-            id: Date.now(),
-            name: state.name,
-            age: state.age,
-            address: state.address
-        }
-        arr.push(data);
-        console.log(arr);
-
-        // arr.push(state);
-        // console.log(arr);
-        // console.log(state)
-        // const data = {name,age,address}; //-->Data in object
-        // console.log(data);
-        // dispatch(setUserData(state));
-    }
-
-    */
-
     let movies = [];
     const addMovie = () => {
         let movie = {
