1f9638b7c17d91c1087b0580b84f971b852517e7
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-28 01:14:21 +0500

    Add Search component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddData from './components/AddData';
 import ListData from './components/ListData';
 import Clock from './components/Clock';
+import SearchData from './components/SearchData';
 
 function App() {
   return (
@@ -10,10 +11,13 @@ function App() {
     <div className="App">
      <AddData/>
     </div>
-    <div className="App">
+    <div className="App" style={{ paddingTop:50 }}>
+     <SearchData/>
+    </div>
+    <div className="App" style={{ paddingTop:50 }}>
      <ListData/>
     </div>
-    <div className="App">
+    <div className="App" style={{ paddingTop:100 }}>
      <Clock/>
     </div>
     </>
