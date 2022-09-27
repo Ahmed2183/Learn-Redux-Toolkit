01e8f7a91dce21ac890681f6c38a39ea6305267c
ranaahmedraza2017 <ranaahmedraza2017@gmail.com>
2022-09-28 01:14:52 +0500

    Searching data

--- src/components/SearchData.js
+++ src/components/SearchData.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 
 const SearchData = () => {
+
+  const { userData } = useSelector((state) => state.userReducer);
+  // console.log(userData);
+
+  const [searchdata, setSearchdata] = useState(userData);
+
+  const filterData = e => {
+    const search = e.target.value.toLowerCase();
+    const filteredData = userData?.filter(item => item?.name.toLowerCase().includes(search));
+    setSearchdata(filteredData);
+  }
+
   return (
-    <div>SearchData</div>
+    <div>
+      <input
+        type="text"
+        name="search"
+        placeholder="Search Data"
+        onChange={(e) => filterData(e)}
+      />
+
+      {
+        searchdata?.length === undefined ? '' :
+          <>
+            {searchdata?.map((user) =>
+              <div key={user.id}>
+                <div>
+                  <label>Name: {user?.name}</label>
+                </div>
+                <div>
+                  <label>Age: {user?.age}</label>
+                </div>
+              </div>
+            )
+            }
+          </>
+      }
+    </div>
   )
 }
 
